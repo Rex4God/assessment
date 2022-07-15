9c4709ce1828d6a051edc2a6789cfeb3ac38f04b
Agamuyi Precious Edobor <preciousagamuyi@gmail.com>
2022-07-15 06:24:43 +0100

    An assessment that add up two numbers to get a target number

---
+++ assessment.js
@@ -0,0 +1,18 @@
+const twoSum =(nums, target)=>{
+    var sums =[];
+    for(var i = 0 ; i < nums.length; i++){
+
+        for(var j = i +1; j < nums.length; j++){
+
+            if(nums[i] + nums[j] ===target){
+
+                sums.push(nums[i], nums[j]);
+            };
+        };
+    };
+    return sums
+};
+
+console.log(twoSum([2,7,11,15],9))
+console.log(twoSum([3, 2, 4],7))
+console.log(twoSum([3,3],6))
