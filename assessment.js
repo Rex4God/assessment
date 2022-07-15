8a284db93c82ab2d8c7cacb5aa007889ea796475
Agamuyi Precious Edobor <preciousagamuyi@gmail.com>
2022-07-15 08:43:01 +0100

    added expected output example to the solution

--- assessment.js
+++ assessment.js
@@ -13,6 +13,6 @@ const twoSum =(nums, target)=>{
     return sums
 };
 
-console.log(twoSum([2,7,11,15],9))
-console.log(twoSum([3, 2, 4],7))
-console.log(twoSum([3,3],6))
+console.log(twoSum([2,7,11,15],9)) // expected output [2,7]
+console.log(twoSum([3, 2, 4],7))   // expected output [3,4]
+console.log(twoSum([3,3],6))       // expected output [3,3]
